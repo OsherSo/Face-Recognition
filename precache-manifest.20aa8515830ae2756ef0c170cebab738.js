5612b15432cffe4c8b55ffbd746694f85549c217
Osher Solimany <solimani.osher@gmail.com>
2019-12-23 22:54:21 +0200

    Updates

---
+++ precache-manifest.20aa8515830ae2756ef0c170cebab738.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6ef2260e9daf0cef28a59525d3103d",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "657ca44f8b614093f130",
+    "url": "/Face-Recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a0eb4b8ceeac3bd1b9c9",
+    "url": "/Face-Recognition/static/css/main.472187c8.chunk.css"
+  },
+  {
+    "revision": "657ca44f8b614093f130",
+    "url": "/Face-Recognition/static/js/2.bdd808c0.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Face-Recognition/static/js/2.bdd808c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0eb4b8ceeac3bd1b9c9",
+    "url": "/Face-Recognition/static/js/main.5d0fa660.chunk.js"
+  },
+  {
+    "revision": "0f0073e905167f597c8c",
+    "url": "/Face-Recognition/static/js/runtime-main.8c688425.js"
+  }
+]);
